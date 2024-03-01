9446ddd8cff27e8e56a144381a6f8c6d2942d8e8
Despi <D.Ioannide2@newcastle.ac.uk>
2024-03-01 17:42:49 +0000

    Added Doc strings and comments for better code understanding and clarity in order to begin improving the speed and accuracy of my algorithm.

--- main.py
+++ main.py
@@ -12,27 +12,50 @@ TARGET.load()  # read image and close the file
 
 # Make the polygon and randomise the coordinates of the polygon corners
 def make_polygon(n):
-    R, G, B = [random.randrange(256) for i in range(3)]
-    A = random.randrange(30, 60)
-    x1, y1, x2, y2, x3, y3 = [random.randrange(10, 190) for i in range(6)]
+    """
+    Randomly assigns the colour, opaqueness and coordinates of the polygons in this instance
+     I chose to use triangles because I believe they will be an easier shape to work with
+    """
+    R, G, B = [random.randrange(256) for i in range(3)]  # Assigns random int values to Red, Green and Blue
+    A = random.randrange(30, 60)  # Sets random opaqueness
+    x1, y1, x2, y2, x3, y3 = [random.randrange(10, 190) for i in range(6)]  # Sets random coordinates to polygons
 
     # 0 <= R|G|B < 256, 30 <= A <= 60, 10 <= x|y < 190
     return [(R, G, B, A), (x1, y1), (x2, y2), (x3, y3)]
 
 
 def initialise():
+    """Assigns the sides of the polygon and creates as many polygons as stated in the constants above"""
     return [make_polygon(SIDES) for i in range(POLYGON_COUNT)]
 
 
 def draw(solution):
+    """
+    Parameters: Solution - which contains the 100 polygons generated in initialise().
+    Create a blank picture: Make a new, empty picture that's colored and 200x200 pixels big.
+    Draw Shapes: using the images colour and outline
+    Returns: The picture with all the shapes drawn on it."""
     image = Image.new("RGB", (200, 200))
     canvas = ImageDraw.Draw(image, "RGBA")
-    for polygon in solution:
+    for polygon in solution:  # For each shape in a list, draw it on the picture using its specific color and outline.
         canvas.polygon(polygon[1:], fill=polygon[0])
     return image
 
 
 def mutate(solution, rate):
+    """
+    Mutates a given solution of polygons by either adjusting their coordinates slightly or by shuffling the order of polygons.
+    The mutation is randomly applied  with a 50% chance, it modifies the coordinates of a randomly selected polygon.
+    If a point's coordinate is selected for mutation, it keeps it within the boundaries.
+
+    Alternatively, with a 50% chance, the function shuffles the order of all polygons in the solution.
+
+    Parameters:
+    - solution (list of lists): A list where each element represents a polygon.
+    - rate (float): The mutation rate
+    Returns:
+    - list: The mutated solution, which may have one polygon's coordinates slightly adjusted or the order of polygons shuffled.
+    """
     solution = list(solution)
 
     if random.random() < 0.5:  # Should be a value between 0 and 1 (percentage)
@@ -54,14 +77,28 @@ def mutate(solution, rate):
 
 
 def select(population):
+    """Select population randomly, chooses 2 parents"""
     return [random.choice(population) for i in range(2)]
 
 
 def combine(*parents):
+    """Combines the parents chosen in select to create a child"""
     return [a if random.random() < 0.5 else b for a, b in zip(*parents)]
 
 
 def evaluate(solution):
+    """
+    Parameter:
+        Solution - Generated polygons
+
+    1. Compares two pictures to see where they don't match.
+    2. Turns the differences into a simple black and white picture (That is what the "L" stands for).
+    3. Calculates how much the pictures don't match by adding up the differences.
+    4. The closer the score is to 1, the more the pictures are alike.
+
+    Returns:
+         how similar the generated image is to the original picture
+    """
     image = draw(solution)
     diff = ImageChops.difference(image, TARGET)
     hist = diff.convert("L").histogram()
@@ -69,16 +106,26 @@ def evaluate(solution):
     return (MAX - count) / MAX
 
 
-def evolve(population, args):
-    population.survive(fraction=0.2)
+def evolve(population, *args):
+    """
+    1. Adjusts the survival rate of the population
+    2. Creates children by choosing parents and combining them
+    3. Mutates the polygons and sets the rate of mutation
+
+    Returns:
+        the population after all the changes
+    """
+    # Luck = If True, individuals randomly survive based on fitness; defaults to False.
+    population.survive(fraction=0.5, luck=True)
     population.breed(parent_picker=select, combiner=combine)
-    population.mutate(mutate_function=mutate, rate=0.9)
+    population.mutate(mutate_function=mutate, rate=0.5)  # Mutates the polygons to and sets mutation rate
     return population
 
 
-population = Population.generate(initialise, evaluate, size=10, maximize=True)
-for i in range(6000):
-    evolve(population, evaluate)
-    print(i, evaluate(solution=population[0].chromosome))
+if __name__ == "__main__":
+    population = Population.generate(initialise, evaluate, size=10, maximize=True)
+    for i in range(6000):
+        evolve(population)  # returns the evolved population.
+        print(i, evaluate(solution=population[0].chromosome))
 
-draw(population[0].chromosome).save("solution.png")
+    draw(population[0].chromosome).save("solution.png")
